e4f8d1735c0518d863db4dda53b148d910044ced
zhangjianhua <zhangjianhua@chehejia.com>
2020-10-20 17:58:59 +0800

    delete no use code.

--- src/main/java/run/halo/app/Application.java
+++ src/main/java/run/halo/app/Application.java
@@ -19,26 +19,9 @@ public class Application  {
     private static ConfigurableApplicationContext context;
 
     public static void main(String[] args) {
-        // Customize the spring config location
-        System.setProperty("spring.config.additional-location", "file:${user.home}/.halo/,file:${user.home}/halo-dev/");
-
         // Run application
         context = SpringApplication.run(Application.class, args);
-
     }
 
-    /**
-     * Restart Application.
-     */
-    public static void restart() {
-        ApplicationArguments args = context.getBean(ApplicationArguments.class);
 
-        Thread thread = new Thread(() -> {
-            context.close();
-            context = SpringApplication.run(Application.class, args.getSourceArgs());
-        });
-
-        thread.setDaemon(false);
-        thread.start();
-    }
 }
